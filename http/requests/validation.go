40c2ecaf43c29bad69638e801d5d99b2c721af49
SystemGlitch <jeremy.la@outlook.fr>
2019-11-21 20:31:01 +0100

    Add comments with links to docs

--- http/requests/validation.go
+++ http/requests/validation.go
@@ -1,5 +1,23 @@
 package requests
 
+// If none of the available validation rules satisfy your needs, you can implement custom validation rules.
+// https://system-glitch.github.io/goyave/guide/basics/validation.html#custom-rules
+
+// import "github.com/System-Glitch/goyave/validation"
+
+//    func validateCustomFormat(field string, value interface{}, parameters []string, form map[string]interface{}) bool {
+//        // Ensure the rule has at least one parameter
+//        validation.RequireParametersCount("custom_format", parameters, 1)
+//        str, ok := value.(string)
+//
+//        if ok { // The data under validation is a string
+//            return regexp.MustCompile(parameters[0]).MatchString(str)
+//       }
+//
+//        return false // Cannot validate this field
+//    }
+
 func init() {
 	// Register your custom validation rules here.
+	// validation.AddRule("custom_format", false, validateCustomFormat)
 }
--- kernel.go
+++ kernel.go
@@ -1,12 +1,22 @@
 package main
 
 import (
-	"github.com/System-Glitch/goyave"
 	"goyave_template/http/routes"
 
+	"github.com/System-Glitch/goyave"
+
+	// Import the approriate GORM dialect for the database you're using.
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	// import _ "github.com/jinzhu/gorm/dialects/postgres"
+	// import _ "github.com/jinzhu/gorm/dialects/sqlite"
+	// import _ "github.com/jinzhu/gorm/dialects/mssql"
 )
 
 func main() {
+	// This is the entry point of your application.
+	// Most applications don't need more than this, but
+	// if you are running multiple services, such as a
+	// websocket server, you'll need to run Goyave in a routine.
+	// See: https://system-glitch.github.io/goyave/guide/advanced/multi-services.html
 	goyave.Start(routes.Register)
 }
